7608acd820a8fd4e349363f7b4643056de6a682d
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-03-13 03:56:30 +0400

    🧹 cleanup languages that we didn't update properly
    prepare to move to different language library

--- data/pages/copy-release-trans.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-targets = ['da', 'es', 'fr', 'hu', 'it', 'ko-KR', 'nl', 'pt-BR', 'pt-PT', 'ro-RO', 'sv', 'tr', 'zh-CN']
-
-for target in targets:
-  print(f"copying en/release -> {target}/release")
-  os.system(f"cp ./en/release {target}/release")
