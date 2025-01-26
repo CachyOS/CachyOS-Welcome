5312af17289b37f0cbdcb4ff7dee67f84fb58010
Christian S. <crian84@gmail.com>
2025-01-26 03:29:59 +0100

    Apply suggestions from code review
    Co-authored-by: Vladislav Nepogodin <nepogodin.vlad@gmail.com>

--- src/pages.rs
+++ src/pages.rs
@@ -391,7 +391,8 @@ fn create_fixes_section(builder: &Builder) -> gtk::Box {
                     })
                     .expect("Couldn't send data to channel");
             } else {
-                for package in packages_to_install {
+                let packages = packages_to_install.join(" ");
+                let _ = utils::run_cmd_terminal(format!("pacman -S {packages}"), true);
                     let _ = utils::run_cmd_terminal(format!("pacman -S {}", package), true);
                 }
            }
