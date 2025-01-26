bd96efd12f757ca7c82e62f4e0b87e4eac07b2f6
Christian S. <crian84@gmail.com>
2025-01-26 03:30:56 +0100

    Apply suggestions from code review

--- src/pages.rs
+++ src/pages.rs
@@ -394,7 +394,6 @@ fn create_fixes_section(builder: &Builder) -> gtk::Box {
                 let packages = packages_to_install.join(" ");
                 let _ = utils::run_cmd_terminal(format!("pacman -S {packages}"), true);
                     let _ = utils::run_cmd_terminal(format!("pacman -S {}", package), true);
-                }
            }
         });
     });
