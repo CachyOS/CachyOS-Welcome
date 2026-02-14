9c8711230161996696ac887a342099122b25b753
Tobias <5702338+T0biii@users.noreply.github.com>
2026-02-14 17:20:29 +0100

    Add FFMUC DNS to DnsServer enum and mapping

--- src/dns.rs
+++ src/dns.rs
@@ -61,6 +61,7 @@ pub enum DnsServer {
     YandexMalwareAdult,
     AliDns,
     Tencent,
+    FFMUC,
 }
 
 // TODO(vnepogodin): use these mapping instead of phf::map
@@ -82,6 +83,7 @@ impl DnsServer {
             DnsServer::YandexMalwareAdult => "Yandex Malware and adult content blocking",
             DnsServer::AliDns => "阿里云公共DNS (AliDNS)",
             DnsServer::Tencent => "腾讯云 DNSPod (Tencent)",
+            DnsServer::FFMUC => "FFMUC DNS"
         }
     }
 }
