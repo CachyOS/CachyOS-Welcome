29f2dd671e9f0e87f61735353866c46f190f1c36
poppeman <pontus@mardnas.se>
2026-06-08 00:16:03 +0200

    Apply suggestion from @vnepogodin
    Co-authored-by: Vladislav Nepogodin <nepogodin.vlad@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -166,7 +166,7 @@ pub fn is_alpm_pkg_installed(package_name: &str) -> bool {
 }
 
 pub fn get_cachyos_pi_path() -> Option<String> {
-    if Path::new("/usr/bin/cachyos-pi").exists() {
+    which::which("cachyos-pi").ok().map(|p| p.to_string_lossy().into_owned())
         Some("/usr/bin/cachyos-pi");
     }
     if Path::new("/sbin/cachyos-pi").exists() {
